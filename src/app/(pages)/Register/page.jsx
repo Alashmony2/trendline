4cf52512b38fa77ebabf31868820d3edb2790653
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-10-05 23:48:48 +0300

    add country code and phone number to register page

--- src/app/(pages)/Register/page.jsx
+++ src/app/(pages)/Register/page.jsx
@@ -7,6 +7,28 @@ export default function Register() {
                     <input name="name" className="input input-info w-4/5" type="text" placeholder="Name" />
                     <input name="email" className="input input-info w-4/5" type="email" placeholder="Email" />
                     <input name="password" className="input input-info w-4/5" type="password" placeholder="Password" />
+                    <div className="flex w-4/5 gap-2">
+                        <select
+                            name="countryCode"
+                            className="select select-info w-1/4"
+                            defaultValue="+966"
+                        >
+                            <option value="+966">🇸🇦 +966</option>
+                            <option value="+20">🇪🇬 +20</option>
+                            <option value="+971">🇦🇪 +971</option>
+                            <option value="+974">🇶🇦 +974</option>
+                            <option value="+965">🇰🇼 +965</option>
+                            <option value="+962">🇯🇴 +962</option>
+                            <option value="+961">🇱🇧 +961</option>
+                            <option value="+1">🇺🇸 +1</option>
+                        </select>
+                        <input
+                            name="phone"
+                            className="input input-info w-3/4"
+                            type="tel"
+                            placeholder="Phone Number"
+                        />
+                    </div>
                     <button className="btn btn-info w-4/5 rounded">Register</button>
                 </div>
             </form>

54585e1df0e3546180c5551b1d677060a3e71ae7
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-10-06 15:10:48 +0300

    update navbar

--- src/app/_components/Navbar/Navbar.jsx
+++ src/app/_components/Navbar/Navbar.jsx
@@ -1,48 +1,72 @@
 'use client'
+import Image from "next/image"
 
 export default function Navbar() {
-    return <>
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
+  return (
+    <nav className="navbar bg-white shadow-sm px-4 md:px-10 py-3 sticky top-0 z-50">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* ===== Left Section (Logo + Menu) ===== */}
+        <div className="flex items-center gap-8 navbar-start">
+          {/* Logo */}
+          <a href="/" className="flex items-center gap-2">
+            <Image
+              src="/assets/Layer_1.svg"
+              alt="Logo"
+              width={65.95}
+              height={51}
+              className="object-contain"
+            />
+          </a>
+          {/* ===== Menu ===== */}
+          <ul className="hidden lg:flex gap-6 font-medium text-gray-700">
+            <li className="flex gap-1 items-center">
+              <Image src="/assets/icons/navicon/home 04.svg" alt="Home" width={20} height={20} />
+              <a href="/" className="text-[#8A8A8A]">Home</a>
+            </li>
+            <li className="flex gap-1 items-center">
+              <Image src="/assets/icons/navicon/apps.svg" alt="category" width={20} height={20} />
+              <a href="/" className="text-[#8A8A8A]">Our Category</a>
+            </li>
+            <li className="flex gap-1 items-center">
+              <Image src="/assets/icons/navicon/about.svg" alt="about" width={20} height={20} />
+              <a href="/" className="text-[#8A8A8A]">About Us</a>
+            </li>
+            <li className="flex gap-1 items-center">
+              <Image src="/assets/icons/navicon/contact.svg" alt="contact" width={20} height={20} />
+              <a href="/" className="text-[#8A8A8A]">Contact Us</a>
+            </li>
+            <li className="flex gap-1 items-center">
+              <Image src="/assets/icons/navicon/faq.svg" alt="faq" width={20} height={20} />
+              <a href="/" className="text-[#8A8A8A]">FAQs</a>
+            </li>
+          </ul>
         </div>
-
-    </>
+        {/* ===== Right Section ===== */}
+        <div className="flex items-center gap-4">
+          {/* ===== Icons ===== */}
+          <div className="hidden lg:flex items-center gap-4">
+            <Image src={"/assets/icons/navicon/shopping.svg"} alt="shopping" width={20} height={20} />
+            <Image src={"/assets/icons/navicon/notification.svg"} alt="notification" width={20} height={20} />
+            <Image src={"/assets/icons/navicon/love.svg"} alt="love" width={20} height={20} />
+            <Image src={"/assets/icons/navicon/Frame.svg"} alt="frame" width={98} height={24} />
+          </div>
+          {/* ===== Mobile Menu Button ===== */}
+          <div className="lg:hidden">
+            <div className="dropdown dropdown-end">
+              <Image tabIndex={0} src={"/assets/icons/navicon/toggle.svg"} className="cursor-pointer" alt="toggle" width={24} height={24} />
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-white rounded-box shadow-lg mt-3 right-0 w-52 z-[100]">
+                <li><a>Home</a></li>
+                <li><a>Our Category</a></li>
+                <li><a>About Us</a></li>
+                <li><a>Contact Us</a></li>
+                <li><a>FAQs</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }

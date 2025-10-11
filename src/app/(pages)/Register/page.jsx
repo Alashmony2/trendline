b092ef6d29bba98e3e1e586ca17a9b53fe89cc1f
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-10-11 14:27:11 +0300

    update Register

--- src/app/(pages)/Register/page.jsx
+++ src/app/(pages)/Register/page.jsx
@@ -2,10 +2,12 @@
 import axios from "axios"
 import { useFormik } from "formik"
 import { useRouter } from "next/navigation.js"
+import { useState } from "react"
 import * as Yup from 'yup'
 
 export default function Register() {
-    const router = useRouter()
+    const router = useRouter();
+    const [errorMessage, setErrorMessage] = useState("");
     const initialValues = {
         name: "",
         email: "",
@@ -16,26 +18,26 @@ export default function Register() {
     }
     const onSubmit = async () => {
         try {
-          const { data } = await axios.post(
-            "https://tinytales.trendline.marketing/api/auth/register",
-            formik.values
-          );
-          router.push("/VerifyAccount")
+            const { data } = await axios.post(
+                "https://tinytales.trendline.marketing/api/auth/register",
+                formik.values
+            );
+            router.push("/VerifyAccount")
         } catch (error) {
-          console.log("Register error:", error.response?.data || error.message);
+            setErrorMessage(error.response?.data?.message || "Registration failed");
         }
-      };
-      
+    };
+
     const validationSchema = Yup.object({
         name: Yup.string().required("Name is required"),
         email: Yup.string().email("Invalid email").required("Email is required"),
         password: Yup.string()
-        .min(8, "Password must be at least 8 characters")
-        .matches(/[a-z]/, "Password must contain at least one lowercase letter")
-        .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
-        .matches(/\d/, "Password must contain at least one number")
-        .matches(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character")
-        .required("Password is required"),        password_confirmation: Yup.string().oneOf([Yup.ref("password"), null], "Passwords must match").required("Confirm Password is required"),
+            .min(8, "Password must be at least 8 characters")
+            .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+            .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+            .matches(/\d/, "Password must contain at least one number")
+            .matches(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character")
+            .required("Password is required"), password_confirmation: Yup.string().oneOf([Yup.ref("password"), null], "Passwords must match").required("Confirm Password is required"),
         mobile_country_code: Yup.string().required("Mobile Country Code is required"),
         mobile: Yup.string().required("Mobile is required"),
     })
@@ -45,17 +47,26 @@ export default function Register() {
         validationSchema
     })
     return (
-        <div className="sm:w-2/3 mx-auto">
+        <div className="sm:w-2/3 mx-auto my-4">
             <h1 className="text-3xl text-center font-bold">Register Now</h1>
             <form onSubmit={formik.handleSubmit}>
                 <div className="py-5 flex flex-wrap gap-4 justify-center">
                     <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.name} name="name" className="input input-info w-4/5" type="text" placeholder="Name" />
-                    {formik.errors.name && formik.touched.name && <p className="text-red-500">{formik.errors.name}</p>}<input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} name="email" className="input input-info w-4/5" type="email" placeholder="Email" />
-                    {formik.errors.email && formik.touched.email && <p className="text-red-500">{formik.errors.email}</p>}
+                    {formik.errors.name && formik.touched.name && <div role="alert" className="alert alert-error alert-soft w-4/5">
+                        <span>{formik.errors.name}</span>
+                    </div>}
+                    <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} name="email" className="input input-info w-4/5" type="email" placeholder="Email" />
+                    {formik.errors.email && formik.touched.email && <div role="alert" className="alert alert-error alert-soft w-4/5">
+                        <span>{formik.errors.email}</span>
+                    </div>}
                     <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.password} name="password" className="input input-info w-4/5" type="password" placeholder="Password" />
-                    {formik.errors.password && formik.touched.password && <p className="text-red-500">{formik.errors.password}</p>}
+                    {formik.errors.password && formik.touched.password && <div role="alert" className="alert alert-error alert-soft w-4/5">
+                        <span>{formik.errors.password}</span>
+                    </div>}
                     <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.password_confirmation} name="password_confirmation" className="input input-info w-4/5" type="password" placeholder="Confirm Password" />
-                    {formik.errors.password_confirmation && formik.touched.password_confirmation && <p className="text-red-500">{formik.errors.password_confirmation}</p>}
+                    {formik.errors.password_confirmation && formik.touched.password_confirmation && <div role="alert" className="alert alert-error alert-soft w-4/5">
+                        <span>{formik.errors.password_confirmation}</span>
+                    </div>}
                     <div className="flex w-4/5 gap-2">
                         <select onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.mobile_country_code}
                             name="mobile_country_code"
@@ -76,9 +87,15 @@ export default function Register() {
                             type="tel"
                             placeholder="Phone Number"
                         />
-                        {formik.errors.mobile && formik.touched.mobile && <p className="text-red-500">{formik.errors.mobile}</p>}
+                        {formik.errors.mobile && formik.touched.mobile && <div role="alert" className="alert alert-error alert-soft w-4/5">
+                            <span>{formik.errors.mobile}</span>
+                        </div>}
                     </div>
                     <button className="btn btn-info w-4/5 rounded">Register</button>
+                    <p className="text-gray-400 font-medium">Do you have an account <a href="/Login" className="text-[#38C9FE]">Login</a></p>
+                    {errorMessage && <div role="alert" className="alert alert-error alert-soft w-4/5">
+                        <span>{errorMessage}</span>
+                    </div>}
                 </div>
             </form>
         </div>

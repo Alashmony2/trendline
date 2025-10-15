587451a93231fb60f0399ede0294ccfbb92a0d36
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-10-15 23:27:52 +0300

    update main row (right 'info')

--- src/app/(pages)/ProductDetails/page.jsx
+++ src/app/(pages)/ProductDetails/page.jsx
@@ -72,38 +72,44 @@ export default function ProductDetails() {
                     {/* Right: Info */}
                     <div className="flex-1 space-y-5 w-full ">
                         <div className="flex items-center justify-between">
-                            <span className="border text-[#b08a6d] px-3 py-1 rounded-full text-sm font-medium">
+                            <span className="border border-[#BE968E] text-[#BE968E] px-3 py-1 rounded-full text-sm font-semibold">
                                 T-Shirt
                             </span>
-                            <button className="p-2 border rounded-md hover:bg-gray-100 ">
-                                <Heart className="text-[#b08a6d] " />
-                            </button>
+                            <div className="space-x-1">
+                                <button className="p-2 border border-[#4040401A] rounded-md hover:bg-gray-100 ">
+                                    <Image width={32} height={32} src="/assets/home/bag.svg" alt="bag" />
+                                </button>
+                                <button className="p-2 border border-[#4040401A] rounded-md hover:bg-gray-100 ">
+                                    <Image width={32} height={32} src="/assets/home/love.svg" alt="heart" />
+                                </button>
+                            </div>
+
                         </div>
 
-                        <h2 className="text-2xl font-medium text-gray-800">
+                        <h2 className="text-2xl font-medium text-[#020202]">
                             J.VER Man Shirts Solid Long Sleeve Stretch Wrinkle-Free With Blue
                         </h2>
 
                         <div>
-                            <span className="text-xl font-bold ">$300.00</span>
-                            <span className="text-gray-400 line-through ml-2">$360.00</span>
-                            <p className="text-gray-500 text-sm">This price is exclusive of taxes.</p>
+                            <span className="text-xl font-medium ">$300.00</span>
+                            <span className="text-[#8A8A8A]  line-through ml-2">$360.00</span>
+                            <p className="text-[#333333] text-[12px]">This price is exclusive of taxes.</p>
                         </div>
 
-                        <p className="text-gray-500">
+                        <p className="text-[#020202] ">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                         <hr className="text-[#E6E6E6]"></hr>
                         {/* Type & Size */}
-                        <div className="flex flex-col gap-6 w-full max-w-sm">
+                        <div className="flex flex-col justify-center items-center gap-6 w-full max-w-sm">
                             {/* Type */}
-                            <div className="relative w-full">
-                                <label className="absolute -top-2 left-4 bg-white text-gray-600 text-sm px-2">
+                            <div className="relative w-full ">
+                                <label className="absolute -top-2 left-9 md:left-4 bg-white text-[#020202] text-sm px-2 ">
                                     Type
                                 </label>
                                 <select
                                     defaultValue="Cotton"
-                                    className="block w-4/5 p-3 border border-gray-300 rounded-lg text-gray-700 text-base"
+                                    className="block mx-auto md:mx-0 w-6/7 p-3 rounded-[10px] border border-[#00000026] text-[#020202] text-[12px] font-medium"
                                 >
                                     <option>Cotton</option>
                                     <option>Polyester</option>
@@ -112,12 +118,12 @@ export default function ProductDetails() {
 
                             {/* Size */}
                             <div className="relative w-full">
-                                <label className="absolute -top-2 left-4 bg-white text-gray-600 text-sm px-2">
+                                <label className="absolute -top-2 left-9 md:left-4 bg-white text-[#020202] text-sm px-2">
                                     Size
                                 </label>
                                 <select
                                     defaultValue="2XL"
-                                    className="block w-4/5 p-3 border border-gray-300 rounded-lg text-gray-700 text-base"
+                                    className="block mx-auto md:mx-0 w-6/7 p-3 rounded-[10px] border border-[#00000026] text-[#020202] text-[12px] font-medium"
                                 >
                                     <option>2XL</option>
                                     <option>XL</option>
@@ -130,34 +136,42 @@ export default function ProductDetails() {
                         {/* Colors */}
 
                         <div>
-                            <p className="text-gray-600 text-sm mb-2">Colors</p>
+                            <p className="text-[#020202] text-[20px] font-medium mb-2">Colors</p>
                             <div className="flex gap-3">
                                 {colors.map((c, i) => (
-                                    <div key={i} className="bg-[#F4F7F9] flex p-2 rounded-full">
-                                        <button
-
+                                    <div
+                                        key={i}
+                                        onClick={() => setSelectedColor(c.key)}
+                                        className={`bg-[#F4F7F9] flex p-2 rounded-full cursor-pointer border transition-all duration-200 ${selectedColor === c.key ? "border-[#020202]" : "border-transparent"
+                                            }`}
+                                    >
+                                        <div
                                             aria-label={`color-${c.key}`}
-                                            onClick={() => setSelectedColor(c.key)}
-                                            className={`w-6 h-6 rounded-full border-2 cursor-pointer ${c.class} ${selectedColor === c.key ? " ring-offset-1" : "border-gray-300"
-                                                }`}
-                                        /></div>
+                                            className={`w-6 h-6 rounded-full ${c.class}`}
+                                        />
+                                    </div>
                                 ))}
                             </div>
                         </div>
 
                         {/* Quantity + Add to cart */}
-                        <div className="">
-                            <p className="font-medium text-xl">Quantity <span className="text-[#8A8A8A] text-[16px] font-[400]">($300.00 for Piece)</span></p>
-                            <div className="flex items-center gap-4">
-                                <div className="flex items-center bg-[#F5F5F5] rounded-lg py-2 px-3">
-                                    <button onClick={dec} className="px-3 py-2 bg-white rounded-xl text-gray-600 ">-</button>
-                                    <span className="px-4 min-w-8 text-center tabular-nums">{String(quantity).padStart(2, "0")}</span>
-                                    <button onClick={inc} className="px-3 py-2 bg-white rounded-xl text-black ">+</button>
+                        <div >
+                            <p className="font-medium text-xl text-[#020202]">Quantity <span className="text-[#8A8A8A] text-[16px] font-[400]">($300.00 for Piece)</span></p>
+                            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
+                                <div className="flex items-center justify-around gap-4">
+                                    <div className="flex items-center bg-[#F5F5F5] rounded-lg py-1 px-3 mt-2">
+                                        <button onClick={dec} className="px-3 py-1 bg-white rounded-xl text-gray-600 ">-</button>
+                                        <span className="px-4 min-w-8 text-center tabular-nums">{String(quantity).padStart(2, "0")}</span>
+                                        <button onClick={inc} className="px-3 py-1 bg-white rounded-xl text-black ">+</button>
+                                    </div>
+                                    <p className="text-2xl font-medium">$300.00</p>
                                 </div>
-                                <p className="text-2xl font-medium">$300.00</p>
-                                <button className="bg-[#BE968E] text-white px-9 py-2 rounded-md ">
+                                <div >
+                                    <button className="bg-[#BE968E] block w-5/6 md:w-fit text-white px-9 py-2 rounded-md mx-auto ">
                                     Add To Cart
                                 </button>
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
